5f3eae080a77b8b3803b6914a26419cb1b1f8373
Stefan Kanev <stefan.kanev@gmail.com>
2025-08-22 23:08:23 +0300

    Third time is the charm

--- DwxJJ/keymap.c
+++ DwxJJ/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_ENTER,       
     MT(MOD_LCTL, KC_ESCAPE),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TRANSPARENT,                                                                 MU_NEXT,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MU_NEXT,        
     OSM(MOD_LSFT),  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       OSM(MOD_RSFT),  
-    QK_REPEAT_KEY, KC_TRANSPARENT, KC_LEFT_GUI,    MO(5),          MO(3),          QK_REPEAT_KEY,                                                                                                  QK_REPEAT_KEY, KC_ESCAPE),MO(4),          LT(6, KC_ENTER),KC_TRANSPARENT, KC_TRANSPARENT, MO(2),          
+    QK_REPEAT_KEY, KC_TRANSPARENT, KC_LEFT_GUI,    MO(5),          MO(3),          QK_REPEAT_KEY,                                                                                                  QK_REPEAT_KEY,MO(4),          LT(6, KC_ENTER),KC_TRANSPARENT, KC_TRANSPARENT, MO(2),          
     KC_BSPC,        KC_TRANSPARENT, KC_TRANSPARENT,                 KC_ENTER,       KC_LEFT_GUI,    KC_SPACE
   ),
   [1] = LAYOUT_moonlander(
